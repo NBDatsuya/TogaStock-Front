d0058097f96f74cd97f0b23a5a35282e0a13ce97
NBDatsuya <2646809050@qq.com>
2023-06-03 12:42:09 +0800

    feat: HomePage

---
+++ src/pages/mainframe/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import StockList from "../../components/StockList.vue";
+import PersonalInfo from "../../components/PersonalInfo.vue";
+</script>
+
+<template>
+	<personal-info density="0"></personal-info>
+	<stock-list></stock-list>
+</template>
+
+<style scoped>
+
+</style>
